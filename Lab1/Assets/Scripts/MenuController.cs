9da9b4d77c66af1f25e8d7a7961c0c2b538d14ee
junwei <limjunwei567@gmail.com>
2022-05-22 22:36:05 +0800

    finish lab 1

--- Lab1/Assets/Scripts/MenuController.cs
+++ Lab1/Assets/Scripts/MenuController.cs
@@ -22,25 +22,8 @@ public class MenuController : MonoBehaviour
         }
     }
 
-    // public void AppearRestartButton() {
-    //     foreach (Transform eachChild in transform) {
-    //         if (eachChild.GetChild(0).name == "Restart") {
-    //             Debug.Log("Child found. Name: " + eachChild.GetChild(0).name);
-    //             eachChild.gameObject.SetActive(true);
-    //         }
-    //     }    
-    // }
-
     public void RestartButtonClicked() 
     {
-        // foreach (Transform eachChild in transform) {
-        //     if (eachChild.GetChild(0).name == "Restart") {
-        //         Debug.Log("Child found. Name: " + eachChild.GetChild(0).name);
-        //         Time.timeScale = 1.0f;
-        //         eachChild.gameObject.SetActive(false);
-        //         SceneManager.LoadScene("SampleScene");
-        //     }
-        // }
         SceneManager.LoadScene("SampleScene");
         Time.timeScale = 1.0f;
         Debug.Log("Restart!");
---
+++ README.md
@@ -0,0 +1,4 @@
+# 50.033 Foundations of Game Design and Development
+
+Lim Jun Wei 1004379
+
